749c5ab98a3d76c51098085ae9f106b928d7658c
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-03-30 22:54:09 -0300

    Updates

---
+++ precache-manifest.f60616d43cf7496c42213a8b1d60245c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e8c0ec44fb7aadcc97e2a8bad80ca6",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "a6fa30835fbfb329572b",
+    "url": "/app-toot-name/static/css/main.b4fa1bed.chunk.css"
+  },
+  {
+    "revision": "a60379915463dbc8f47a",
+    "url": "/app-toot-name/static/js/2.e195886b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-toot-name/static/js/2.e195886b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6fa30835fbfb329572b",
+    "url": "/app-toot-name/static/js/main.cc7e3344.chunk.js"
+  },
+  {
+    "revision": "66ac581fbc8990623b1d",
+    "url": "/app-toot-name/static/js/runtime-main.cfc30c8d.js"
+  }
+]);
